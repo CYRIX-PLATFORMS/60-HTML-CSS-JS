4c16855b628dc5f95bab2884b14cadbd6c03b9c6
CYRIX-CCS <akinimbomnkwi@gmail.com>
2023-05-10 09:15:42 +0100

    renamed

---
+++ Age-Calculator/index.js
@@ -0,0 +1,30 @@
+const button = document.getElementById('btn');
+const birthDay = document.getElementById('birthday');
+ 
+
+function calculateAge() {
+    const birthValue = birthDay.value;
+
+    if (birthValue === ""){
+        alert("Please enter your birthday!!");
+    } else {
+        console.log(getAge(birthValue));
+
+    }
+
+}
+
+
+function getAge(birthValue) {
+    const currentDate = new Date();
+    const birthDate = new Date(birthValue);
+    const age = currentDate.getFullYear() - birthDate.getFullYear();
+    const month = currentDate.getMonth() - birthDate.getMonth();
+
+    if (month < 0 || (month === 0 && currentDate.getDate() < birthDate.getDate())){
+        age = age - 1
+    }
+        return age;
+}
+
+button.addEventListener('click', calculateAge)
