cc7bee2bf414b7c953a4b95a92fb0c3c4ed4e363
CYRIX-CCS <akinimbomnkwi@gmail.com>
2023-05-11 16:10:37 +0100

    test

--- Tip-Calculator/index.js
+++ Tip-Calculator/index.js
@@ -2,3 +2,16 @@ const button = document.getElementById('btn');
 const tip = document.getElementById('tip');
 const bill = document.getElementById('bill');
 
+function calculateTip() {
+
+    let newBill = bill.value;
+    let newTip = tip.value;
+
+    if (newTip < 15 || newTip > 20) {
+        alert("Invalid range tip!!")
+    } else {
+        alert("Valid range tip1");
+    }
+}
+
+button.addEventListener('click',calculateTip);
