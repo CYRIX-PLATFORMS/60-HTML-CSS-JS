860efd6a655fe6ed5bade4b925249a199fb9e0b6
CYRIX-CCS <akinimbomnkwi@gmail.com>
2023-05-16 20:20:56 +0100

    API call from spoonacular to return random recipes

---
+++ Recipe-App/index.js
@@ -0,0 +1,23 @@
+// storing API key for later use
+
+const API_KEY = "eff7aaa4ae7743548683409ec4e44052";
+
+async function getRecipe() {
+
+    const response = await fetch (`https://spoonacular.com/recipes/random?number=10&apikey=${API_KEY}`);
+
+    const data = await response.json();
+
+    return data.recipes;
+
+}
+
+
+
+async function init() {
+
+    const recipes = await getRecipe();
+    console.log(recipes);
+}
+
+init()
