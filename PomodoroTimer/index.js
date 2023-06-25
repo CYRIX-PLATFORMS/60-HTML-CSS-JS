efeee4b8abec68063d514fd5ef7e1bc0683cc61a
CYRIX-CCS <akinimbomnkwi@gmail.com>
2023-06-25 21:49:38 +0100

    start button fully functional

---
+++ PomodoroTimer/index.js
@@ -0,0 +1,42 @@
+const start = document.getElementById("start");
+const stop = document.getElementById("stop");
+const reset = document.getElementById("reset");
+const min = document.getElementById("min");
+const sec = document.getElementById("sec");
+const timer = document.getElementById("timer");
+
+let interval;
+let timeLeft = 1500;
+
+function updateTimer() {
+  let minutes = Math.floor(timeLeft / 60);
+  let seconds = timeLeft % 60;
+  let formattedTime = `${minutes.toString().padStart(2, "0")}:${seconds
+    .toString()
+    .padStart(2, "0")}`;
+
+  timer.innerHTML = formattedTime;
+}
+
+function startTimer() {
+  interval = setInterval(() => {
+    timeLeft--;
+    updateTimer();
+    if (timeLeft === 0) {
+        clearInterval(interval)
+        alert("Time is up!!")
+        timeLeft = 1500
+    }
+  }, 1000);
+}
+function stopTimer() {
+    clearInterval(interval)
+    timeLeft = 1500
+}
+function resetTimer() {
+    
+}
+
+start.addEventListener("click", startTimer);
+stop.addEventListener("click", stopTimer);
+reset.addEventListener("click", resetTimer);
